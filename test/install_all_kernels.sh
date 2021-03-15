61181d83df25a0b3f33a581f78c8d20074fec540
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2021-03-15 20:27:39 +0100

    Also remove installation from CI

--- test/install_all_kernels.sh
+++ test/install_all_kernels.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-./install_kernel.sh adder
+for project in adder:
+do
+  rm -rf build-$project
+  mkdir build-$project
+  cd build-$project
+  cmake -DCMAKE_INSTALL_PREFIX=../inst ../projects/$project
+  make
+  make install_kernelspec
+  cd ..
+done
