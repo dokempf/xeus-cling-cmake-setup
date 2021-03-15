11d5562061f96b8d3a29a18601cabccbcb95f097
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2021-03-15 20:44:17 +0100

    ci fixes

--- test/install_all_kernels.sh
+++ test/install_all_kernels.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-for project in adder:
+set -e
+
+for project in adder
 do
   rm -rf build-$project
   mkdir build-$project
   cd build-$project
-  cmake -DCMAKE_INSTALL_PREFIX=../inst ../projects/$project
+  cmake ../projects/$project
   make
   make install_kernelspec
   cd ..
